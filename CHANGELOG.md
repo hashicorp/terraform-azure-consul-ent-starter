97a2866d553666220cef3f8e9e522622e8dc5dfc
troyready <troy@troyready.com>
2022-01-10 15:08:48 -0800

    switch to user_data
    [Latest TF provider](https://github.com/hashicorp/terraform-provider-azurerm/blob/v2.91.0/CHANGELOG.md#2910-january-07-2022)
    supports the newer
    [user_data](https://docs.microsoft.com/en-us/azure/virtual-machines/user-data)
    API.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## Unreleased
+* Switch VM configuration to use user_data instead of custom_data
+
+## 0.1.0 (October 29, 2021)
+
+* Initial release
--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.15"
 
   required_providers {
-    azurerm = ">=2.0"
+    azurerm = ">=2.91"
     random  = ">=1.0"
   }
 }
